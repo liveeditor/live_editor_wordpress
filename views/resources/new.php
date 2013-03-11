b48fcdf31178f28591f2d5f85dd2b777baefaad3
Chris Peters <chris@clearcrystalmedia.com>
2013-03-11 08:24:47 -0400

    Link Live Editor icon in resources/new

--- views/resources/new.php
+++ views/resources/new.php
@@ -5,7 +5,7 @@
   <?php require_once ABSPATH . "wp-content/plugins/live_editor_files_wordpress/views/layouts/global_navigation.php" ?>
   <div class="media-frame-title">
     <h1>
-      <i class="live-editor icon"></i>
+      <a href="http://www.liveeditorcms.com/?utm_source=WordPress%2BPlugin&amp;utm_medium=link&amp;utm_term=header%2Bicon&amp;utm_content=v0.1&amp;utm_campaign=WordPress%2BPlugin" title="Live Editor"><i class="live-editor icon"></i></a>
       New File
     </h1>
   </div>
